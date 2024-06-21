4cd13f36df56063318b17247d96c3b977679c656
Berlis Rodriguez <Berlisy2j@hotmail.com>
2024-06-20 21:14:52 -0400

    Agrando mas metodos a la clase utilitaria de test

--- src/test/java/com/bersoft/crudapplication/util/TestUtil.java
+++ src/test/java/com/bersoft/crudapplication/util/TestUtil.java
@@ -4,7 +4,9 @@ import com.bersoft.crudapplication.model.domain.Employee;
 import com.bersoft.crudapplication.model.request.EmployeeRequest;
 import com.bersoft.crudapplication.repository.EmployeeRepository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class TestUtil {
     public static EmployeeRepository getEmployeeRepositoryWithDatasourcePopulated(){
@@ -15,11 +17,25 @@ public class TestUtil {
         return  new EmployeeRepository(new HashMap<>());
     }
 
-    public static Employee getEmployee(){
+    public static Employee getEmployeeWithoutId(){
+        return new Employee(null, "JOHN", "CENA", "USA", 63, 67000D, "77777777");
+    }
+
+    public static Employee getEmployeeWithId(){
         return new Employee(null, "JOHN", "CENA", "USA", 63, 67000D, "77777777");
     }
 
     public static EmployeeRequest getEmployeeRequest(){
         return new EmployeeRequest("JOHN", "CENA", "USA", 63, 67000D, "77777777");
+    }
+
+    public static List<Employee> getEmployeeList (){
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add( new Employee(1L, "JOHN", "LENNON", "USA", 19, 3000D, "40212075101"));
+        employeeList.add( new Employee(2L, "CARL", "MAX", "Canada", 49, 3600D, "40212075102"));
+        employeeList.add( new Employee(3L, "HAROLD", "JAMES", "FRANCE", 29, 3500D, "40212075103"));
+        employeeList.add( new Employee(4L, "ANA", "PARKER", "SPAIN", 18, 4000D, "40212075104"));
+        employeeList.add( new Employee(5L, "GEORGE", "CURRY", "HAITI", 37, 7000D, "40212075105"));
+        return employeeList;
     }
 }
